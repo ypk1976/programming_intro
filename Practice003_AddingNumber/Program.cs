edc7cb23e307dd0fa64bc0d916e37be15e3a6148
Dmitry Igdal <ypk1976@gmail.com>
2023-02-23 16:19:30 +0100

    Practice 3 - randomizer added

--- Practice003_AddingNumber/Program.cs
+++ Practice003_AddingNumber/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 5;
-int numberB = 45;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(numberA + " + " + numberB + " = " + result);
