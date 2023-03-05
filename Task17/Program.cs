65363ae52e58d4fedffe5d6e662cc9585f609802
Dmitry Igdal <ypk1976@gmail.com>
2023-03-05 14:11:04 +0100

    Tasks 17 and 18 added

---
+++ Task17/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая принимает на вход координаты точки X и Y, неравные 0. 
+// Необходимо указать номер четверти, в которой находится точка
+Console.WriteLine("Введите значение X");
+int coordX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значени Y");
+int coordY = Convert.ToInt32(Console.ReadLine());
+string errorMessage = "Введенное значение равно 0";
+
+//Method 1
+int chooseQuarter = ChooseQuarter(coordX,coordY);
+string Result = chooseQuarter > 0 
+                        ? $"Введенная координата находится в четверти {chooseQuarter}"
+                        : "Введена неверная координата";
+Console.WriteLine(Result);
+int ChooseQuarter(int x1, int y1)
+{
+    if (x1 > 0 && y1 > 0) return 1;
+    if (x1 > 0 && y1 < 0) return 2;
+    if (x1 < 0 && y1 < 0) return 3;
+    if (x1 < 0 && y1 > 0) return 4;
+    return 0;
+
+}
+
+//Method 2
+CheckValidity(coordX, coordY);
+bool CheckValidity(int numX, int numY)
+{
+    if (numX != 0 && numY != 0)
+    {
+        ProcessParameters(numX, numY);
+        return true;
+    }
+    else
+    {
+        Console.WriteLine(errorMessage);
+        return false;
+    }
+}
+
+int ProcessParameters(int x, int y)
+{
+    int result = 0;
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            result = 1;
+        }
+        else
+        {
+            result = 4;
+        }
+    }
+    else
+    {
+        if (y > 0)
+        {
+            result = 2;
+        }
+        else
+        {
+            result = 3;
+        }
+    }
+    Console.WriteLine(result);
+    return result;
+}
---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая по номеру введенной четверти
+// показывает диапозон возможных координат Х и Y в этой четверти
+Console.WriteLine("Введите номер четверти в системе координат");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string checkQuarter = CheckQuarter(quarter);
+string Result = quarter < 5 && quarter > 0
+                        ? $"В указанной четверти {checkQuarter}"
+                        : "Введен неверный номер четверти";
+Console.WriteLine(Result);
+
+string CheckQuarter(int getQuarter)
+{
+    string errorMessage = "Введен неверный номер четверти!";
+    if (getQuarter == 1) return "X > 0, Y > 0";
+    if (getQuarter == 2) return "X < 0, Y > 0";
+    if (getQuarter == 3) return "X < 0, Y < 0";
+    if (getQuarter == 4) return "X > 0, Y < 0";
+    return errorMessage;
+}
