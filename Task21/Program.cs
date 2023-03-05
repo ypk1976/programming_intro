497cba91cee74ec95ed6d5a63e6b67d45925aafd
Dmitry Igdal <ypk1976@gmail.com>
2023-03-05 17:58:46 +0100

    Tasks 21 & 23 added

---
+++ Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите значение X1");
+int coordX1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значени Y1");
+int coordY1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значени Z1");
+int coordZ1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение X2");
+int coordX2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значени Y2");
+int coordY2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значени Z2");
+int coordZ2 = Convert.ToInt32(Console.ReadLine());
+
+double ourDistance = Distance(coordX1, coordY1, coordZ1, coordX2, coordY2, coordZ2);
+Console.WriteLine(ourDistance);
+
+double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2) );
+    double result = Math.Round(distance, 2, MidpointRounding.ToZero);
+    return result;
+}
---
+++ Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ProcessNumber(number);
+
+bool ProcessNumber(int num)
+{
+    if (num > 0)
+    {
+        GetCube(number);
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Необходимо ввести число больше 0");
+        return false;
+    }
+}
+
+void GetCube(int num)
+{
+    double result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result = Math.Pow(i, 3);
+        Console.WriteLine($"{i,4} -> {result,4}");
+
+    }
+}
